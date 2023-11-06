d20a6d1ba96a67f9f89962e6005b3594535fcca4
Stanislas-Gadeceau <stanislasgadeceau@hotmail.com>
2023-11-06 17:20:02 +0100

    V fonction OK

--- API.py
+++ API.py
@@ -3,35 +3,34 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Dict
 
 app = FastAPI()
 
 # In-memory storage for chat messages
-chat_messages = []
+chat_rooms: Dict[str, list] = {}  # A dictionary to store messages by room name
 
 class Message(BaseModel):
     body: str
     author: str
-   # date : datetime
 
-class Room():
-    def __init__(self) -> None:
-        #self.app = FastAPI()
-        self.messages =[]
+@app.get("/chat/{room_name}")
+def get_chat(room_name: str):
+    if room_name not in chat_rooms:
+        chat_rooms[room_name] = []  # Create the room if it doesn't exist
+    return chat_rooms.get(room_name)
 
-@app.get("/chat")
-def get_chat(room: Room):
-    return room.messages
-
-@app.post("/chat")
-def send_message(message: Message, room: Room):
+@app.post("/chat/{room_name}")
+def send_message(room_name: str, message: Message):
+    if room_name not in chat_rooms:
+        chat_rooms[room_name] = []  # Create the room if it doesn't exist
     timestamp = datetime.now()
     chat_message = {
         "body": message.body,
         "author": message.author,
         "date": timestamp
     }
-    room.messages.append(chat_message)
+    chat_rooms[room_name].append(chat_message)
     return chat_message
 
 if __name__ == "__main__":
